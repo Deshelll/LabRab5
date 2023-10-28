ffa6e8456cf9268f014c8441d29362397ed4ae59
Deshelll <125933466+Deshelll@users.noreply.github.com>
2023-10-29 01:13:04 +0400

    Fourth Commit

--- Lab5/Lab5/AppDbContext.cs
+++ Lab5/Lab5/AppDbContext.cs
@@ -1,12 +1,16 @@
-﻿namespace Test2
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Lab5
 {
-    using Microsoft.EntityFrameworkCore;
 
     public class AppDbContext : DbContext
     {
         public DbSet<FileModel> Files { get; set; }
 
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public int a = 5;
+        public string b = "hello";
     }
 
 }
