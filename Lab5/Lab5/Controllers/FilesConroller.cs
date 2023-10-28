4c714aa7d1907bfe9d98fad73db70d3f20292b38
Deshelll <125933466+Deshelll@users.noreply.github.com>
2023-10-29 00:45:52 +0400

    Third commit

--- Lab5/Lab5/Controllers/FilesConroller.cs
+++ Lab5/Lab5/Controllers/FilesConroller.cs
@@ -17,30 +17,3 @@ public class FilesController : ControllerBase
     {
         _context = context;
     }
-
-    [HttpGet]
-    public async Task<IActionResult> GetFiles()
-    {
-        var files = await _context.Files.ToListAsync();
-        return Ok(files);
-    }
-
-    [HttpGet("{id}")]
-    public async Task<IActionResult> GetFile(int id)
-    {
-        var file = await _context.Files.FindAsync(id);
-        if (file == null)
-        {
-            return NotFound();
-        }
-        return File(file.Data, file.ContentType, file.FileName);
-    }
-
-    [HttpPost]
-    public async Task<IActionResult> UploadFile([FromForm] FileUploadModel fileUpload)
-    {
-        if (fileUpload == null || fileUpload.File == null || fileUpload.File.Length == 0)
-        {
-            return BadRequest("Файл не был загружен.");
-        }
-
