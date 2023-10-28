c5dc77c69d0285e7b3d8a1294a32c5349d70c594
Deshelll <125933466+Deshelll@users.noreply.github.com>
2023-10-29 02:00:52 +0400

    Seven Commit

--- Lab5/Lab5/FileModel.cs
+++ Lab5/Lab5/FileModel.cs
@@ -1,7 +1,5 @@
 ﻿public class FileModel
 {
     public int ID { get; set; }
-    public string? FileName { get; set; }
-    public string? ContentType { get; set; }
-    public byte[]? Data { get; set; }
+
 }
